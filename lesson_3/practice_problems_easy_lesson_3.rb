8af526cdf75761384615da54a549cdf59775c70b
sccr-git <creelins@gmail.com>
2021-08-29 17:39:11 -0400

    practice problem easy, medium and hard in lesson 3

--- lesson_3/practice_problems_easy_lesson_3.rb
+++ lesson_3/practice_problems_easy_lesson_3.rb
@@ -1,1 +1,492 @@
+=begin
+advice = "Few things in life are as important as house training your pet dinosaur."
+
+puts advice.sub!("important", "urgent")
+puts advice
+=end
+
+=begin
+numbers = [1,2,3,4,5]
+
+puts numbers.delete_at(1)
+puts numbers
+=end
+
+=begin
+numbers = [1,2,3,4,5]
+
+puts numbers.delete(1)
+
+puts numbers
+=end
+
+
+#show two different ways to put the expected "Four score and " in front of it.
+
+#famous_words = "seven years ago..."
+
+#famous_words.insert(0, "Four score and ")
+
+# doesn'T work ## famous_words << "Four score and "
+
+#puts "Four score and " << famous_words
+
+#puts "Four score and " + famous_words
+
+#famous_words.prepend("Four score and ")
+
+=begin
+flintstones = ["Fred", "Wilma"]
+flintstones << ["Barnet", "Betty"]
+flintstones << ["BamBam", "Pebles"]
+
+p flintstones.flatten
+=end
+
+=begin
+flintstones = { "Fred" => 0, "Wilma" => 1, "Barney" => 2, "Betty" => 3, "BamBam" => 4, "Pebbles" => 5}
+
+p flintstones.assoc("Barney")
+
+#flintstones.select! {|k,v| v == 2}
+
+#p flintstones.to_a
+=end
+
+=begin
+ages = { "Herman" => 32, "Lily" => 30, "Grandpa" => 402, "Eddie" => 10 }
+
+#puts ages.has_key?('Spot')
+
+#puts ages.include?('Spot')
+
+puts ages.member?('Spot')
+=end
+
+=begin
+munsters_description = "The Munsters are creepy in a good way."
+
+puts munsters_description.swapcase
+
+puts munsters_description.capitalize
+
+puts munsters_description.downcase
+
+puts munsters_description.upcase
+=end
+
+=begin
+ages = { "Herman" => 32, "Lily" => 30, "Grandpa" => 402, "Eddie" => 10 }
+
+additional_ages = { "Marilyn" => 22, "Spot" => 237}
+
+ages.merge!(additional_ages)
+
+#ages["Marilyn"] = 22
+#ages["Spot"] = 237
+
+puts ages
+=end
+
+=begin
+advice = "Few things in life are as important as house training your pet dinosaur."
+
+puts advice.include?('Dino')
+
+p advice.match?("Dino")
+=end
+
+=begin
+flintstones = %w(Fred Barney Wilma Betty BamBAm Pebbles)
+
+#flintstones << "Dino" 
+
+flintstones.concat(%w(Dino Barney))
+
+#flintstones.push("Dino", "Hoppy")
+
+p flintstones
+=end
+
+=begin
+advice = "Few things in life are as important as house training your pet dinosaur."
+
+puts advice.slice!(0, advice.index('house'))
+puts advice
+
+#p advice[0..24]
+=end
+
+=begin
+statement = "The Flintstones Rock!"
+
+puts statement.count('t')
+=end
+
+=begin
+title = "Flintstone Family Members"
+puts title.center(40)
+=end
+
+=begin
+10.times {|number| puts ' ' * number + "The Flintstones Rock!"}
+=end
+
+#puts "the value of 40 + 2 is #{40 + 2} "
+
+=begin
+def factors(number)
+  divisor = number
+  factors = []
+  begin
+    factors << number / divisor if number % divisor == 0
+    divisor -= 1
+  end until divisor == 0
+  factors
+end
+=end
+
+=begin
+def factors(number)
+  divisor = number
+  factors = []
+  while divisor > 0
+    factors << number / divisor if number % divisor == 0
+      divisor -= 1
+  end
+  factors
+end
+
+puts factors(10)
+
+=end
+
+=begin
+def rolling_buffer1(buffer, max_buffer_size, new_element)
+  buffer << new_element
+  buffer.shift if buffer.size > max_buffer_size
+  buffer
+end
+
+def rolling_buffer2(input_array, max_buffer_size, new_element)
+  buffer = input_array + [new_element]
+  buffer.shift if buffer.size > max_buffer_size
+  buffer
+end
+
+#puts rolling_buffer1([0,1,2,3,4,5,6,7,8,9,10,11,12,13,14],10,69)
+
+puts rolling_buffer2([1,2,3,4,5,6,7,8,9], 5, 77)
+=end
+
+=begin
+limit = 15
+
+def fib(first_num, second_num, limit)
+  while first_num + second_num < limit
+    sum = first_num + second_num
+    first_num = second_num
+    second_num = sum
+  end
+  first_num
+  second_num
+end
+
+result = fib(0, 1, limit)
+puts "result is #{result}"
+=end
+
+=begin
+answer = 42
+
+def mess_with_it(some_number)
+  some_number += 8
+end
+
+new_answer = mess_with_it(answer)
+
+p answer - 8
+=end
+
+=begin
+munsters = {
+  "Herman" => { "age" => 32, "gender" => "male" },
+  "Lily" => { "age" => 30, "gender" => "female" },
+  "Grandpa" => { "age" => 402, "gender" => "male" },
+  "Eddie" => { "age" => 10, "gender" => "male" },
+  "Marilyn" => { "age" => 23, "gender" => "female"}
+}
+
+def mess_with_demographics(demo_hash)
+  demo_hash.values.each do |family_member|
+    family_member["age"] += 42
+    family_member["gender"] = "other"
+  end
+end
+
+puts mess_with_demographics(munsters)
+=end
+
+=begin
+def rps(fist1, fist2)
+  if fist1 == "rock"
+    (fist2 == "paper") ? "paper" : "rock"
+  elsif fist1 == "paper"
+    (fist2 == "scissors") ? "scissors" : "paper"
+  else
+    (fist2 == "rock") ? "rock" : "scissors"
+  end
+end
+
+
+puts rps(rps(rps("rock", "paper"), rps("rock", "scissors")), "rock")
+=end
+
+=begin
+def fun_with_ids
+  a_outer = 42
+  b_outer = "forty two"
+  c_outer = [42]
+  d_outer = c_outer[0]
+
+  a_outer_id = a_outer.object_id
+  b_outer_id = b_outer.object_id
+  c_outer_id = c_outer.object_id
+  d_outer_id = d_outer.object_id
+
+  puts "a_outer is #{a_outer} with an id of: #{a_outer_id} before the block."
+  puts "b_outer is #{b_outer} with an id of: #{b_outer_id} before the block."
+  puts "c_outer is #{c_outer} with an id of: #{c_outer_id} before the block."
+  puts "d_outer is #{d_outer} with an id of: #{d_outer_id} before the block."
+  puts
+
+  1.times do
+    a_outer_inner_id = a_outer.object_id
+    b_outer_inner_id = b_outer.object_id
+    c_outer_inner_id = c_outer.object_id
+    d_outer_inner_id = d_outer.object_id
+
+    puts "a_outer id was #{a_outer_id} before the block and is: #{a_outer_inner_id} inside the block."
+    puts "b_outer id was #{b_outer_id} before the block and is: #{b_outer_inner_id} inside the block."
+    puts "c_outer id was #{c_outer_id} before the block and is: #{c_outer_inner_id} inside the block."
+    puts "d_outer id was #{d_outer_id} before the block and is: #{d_outer_inner_id} inside the block."
+    puts
+
+    a_outer = 22
+    b_outer = "thirty three"
+    c_outer = [44]
+    d_outer = c_outer[0]
+
+    puts "a_outer inside after reassignment is #{a_outer} with an id of: #{a_outer_id} before and: #{a_outer.object_id} after."
+    puts "b_outer inside after reassignment is #{b_outer} with an id of: #{b_outer_id} before and: #{b_outer.object_id} after."
+    puts "c_outer inside after reassignment is #{c_outer} with an id of: #{c_outer_id} before and: #{c_outer.object_id} after."
+    puts "d_outer inside after reassignment is #{d_outer} with an id of: #{d_outer_id} before and: #{d_outer.object_id} after."
+    puts
+
+
+    a_inner = a_outer
+    b_inner = b_outer
+    c_inner = c_outer
+    d_inner = c_inner[0]
+
+    a_inner_id = a_inner.object_id
+    b_inner_id = b_inner.object_id
+    c_inner_id = c_inner.object_id
+    d_inner_id = d_inner.object_id
+
+    puts "a_inner is #{a_inner} with an id of: #{a_inner_id} inside the block (compared to #{a_outer.object_id} for outer)."
+    puts "b_inner is #{b_inner} with an id of: #{b_inner_id} inside the block (compared to #{b_outer.object_id} for outer)."
+    puts "c_inner is #{c_inner} with an id of: #{c_inner_id} inside the block (compared to #{c_outer.object_id} for outer)."
+    puts "d_inner is #{d_inner} with an id of: #{d_inner_id} inside the block (compared to #{d_outer.object_id} for outer)."
+    puts
+  end
+
+  puts "a_outer is #{a_outer} with an id of: #{a_outer_id} BEFORE and: #{a_outer.object_id} AFTER the block."
+  puts "b_outer is #{b_outer} with an id of: #{b_outer_id} BEFORE and: #{b_outer.object_id} AFTER the block."
+  puts "c_outer is #{c_outer} with an id of: #{c_outer_id} BEFORE and: #{c_outer.object_id} AFTER the block."
+  puts "d_outer is #{d_outer} with an id of: #{d_outer_id} BEFORE and: #{d_outer.object_id} AFTER the block."
+  puts
+
+  puts "a_inner is #{a_inner} with an id of: #{a_inner_id} INSIDE and: #{a_inner.object_id} AFTER the block." rescue puts "ugh ohhhhh"
+  puts "b_inner is #{b_inner} with an id of: #{b_inner_id} INSIDE and: #{b_inner.object_id} AFTER the block." rescue puts "ugh ohhhhh"
+  puts "c_inner is #{c_inner} with an id of: #{c_inner_id} INSIDE and: #{c_inner.object_id} AFTER the block." rescue puts "ugh ohhhhh"
+  puts "d_inner is #{d_inner} with an id of: #{d_inner_id} INSIDE and: #{d_inner.object_id} AFTER the block." rescue puts "ugh ohhhhh"
+end
+
+fun_with_ids
+=end
+
+=begin
+def fun_with_ids
+  a_outer = 42
+  b_outer = "forty two"
+  c_outer = [42]
+  d_outer = c_outer[0]
+
+  a_outer_id = a_outer.object_id
+  b_outer_id = b_outer.object_id
+  c_outer_id = c_outer.object_id
+  d_outer_id = d_outer.object_id
+
+  puts "a_outer is #{a_outer} with an id of: #{a_outer_id} before the block."
+  puts "b_outer is #{b_outer} with an id of: #{b_outer_id} before the block."
+  puts "c_outer is #{c_outer} with an id of: #{c_outer_id} before the block."
+  puts "d_outer is #{d_outer} with an id of: #{d_outer_id} before the block."
+  puts
+
+  an_illustrative_method(a_outer, b_outer, c_outer, d_outer, a_outer_id, b_outer_id, c_outer_id, d_outer_id)
+
+
+  puts "a_outer is #{a_outer} with an id of: #{a_outer_id} BEFORE and: #{a_outer.object_id} AFTER the method call."
+  puts "b_outer is #{b_outer} with an id of: #{b_outer_id} BEFORE and: #{b_outer.object_id} AFTER the method call."
+  puts "c_outer is #{c_outer} with an id of: #{c_outer_id} BEFORE and: #{c_outer.object_id} AFTER the method call."
+  puts "d_outer is #{d_outer} with an id of: #{d_outer_id} BEFORE and: #{d_outer.object_id} AFTER the method call."
+  puts
+
+  puts "a_inner is #{a_inner} with an id of: #{a_inner_id} INSIDE and: #{a_inner.object_id} AFTER the method." rescue puts "ugh ohhhhh"
+  puts "b_inner is #{b_inner} with an id of: #{b_inner_id} INSIDE and: #{b_inner.object_id} AFTER the method." rescue puts "ugh ohhhhh"
+  puts "c_inner is #{c_inner} with an id of: #{c_inner_id} INSIDE and: #{c_inner.object_id} AFTER the method." rescue puts "ugh ohhhhh"
+  puts "d_inner is #{d_inner} with an id of: #{d_inner_id} INSIDE and: #{d_inner.object_id} AFTER the method." rescue puts "ugh ohhhhh"
+  puts
+end
+
+
+def an_illustrative_method(a_outer, b_outer, c_outer, d_outer, a_outer_id, b_outer_id, c_outer_id, d_outer_id)
+
+  puts "a_outer id was #{a_outer_id} before the method and is: #{a_outer.object_id} inside the method."
+  puts "b_outer id was #{b_outer_id} before the method and is: #{b_outer.object_id} inside the method."
+  puts "c_outer id was #{c_outer_id} before the method and is: #{c_outer.object_id} inside the method."
+  puts "d_outer id was #{d_outer_id} before the method and is: #{d_outer.object_id} inside the method."
+  puts
+
+  a_outer = 22
+  b_outer = "thirty three"
+  c_outer = [44]
+  d_outer = c_outer[0]
+
+  puts "a_outer inside after reassignment is #{a_outer} with an id of: #{a_outer_id} before and: #{a_outer.object_id} after."
+  puts "b_outer inside after reassignment is #{b_outer} with an id of: #{b_outer_id} before and: #{b_outer.object_id} after."
+  puts "c_outer inside after reassignment is #{c_outer} with an id of: #{c_outer_id} before and: #{c_outer.object_id} after."
+  puts "d_outer inside after reassignment is #{d_outer} with an id of: #{d_outer_id} before and: #{d_outer.object_id} after."
+  puts
+
+  a_inner = a_outer
+  b_inner = b_outer
+  c_inner = c_outer
+  d_inner = c_inner[0]
+
+  a_inner_id = a_inner.object_id
+  b_inner_id = b_inner.object_id
+  c_inner_id = c_inner.object_id
+  d_inner_id = d_inner.object_id
+
+  puts "a_inner is #{a_inner} with an id of: #{a_inner_id} inside the method (compared to #{a_outer.object_id} for outer)."
+  puts "b_inner is #{b_inner} with an id of: #{b_inner_id} inside the method (compared to #{b_outer.object_id} for outer)."
+  puts "c_inner is #{c_inner} with an id of: #{c_inner_id} inside the method (compared to #{c_outer.object_id} for outer)."
+  puts "d_inner is #{d_inner} with an id of: #{d_inner_id} inside the method (compared to #{d_outer.object_id} for outer)."
+  puts
+end
+
+fun_with_ids
+end
+=end
+
+=begin
+def tricky_method(a_string_param, an_array_param)
+  a_string_param += "rutabaga"
+  an_array_param << "rutabaga"
+  puts "object id for a_string_param: #{a_string_param.object_id}"
+  puts "object id for an_array_param: #{an_array_param.object_id}"
+end
+
+my_string = "pumpkins"
+my_array = ["pumpkins"]
+tricky_method(my_string, my_array)
+
+puts "My string looks like this now: #{my_string}"
+puts "My array looks like this now: #{my_array}"
+puts "object id for array item: pumpkins #{my_array[0].object_id}"
+puts "object id for array item: rutabaga #{my_array[1].object_id}"
+puts "object id for my_array: #{my_array.object_id}"
+=end
+
+=begin
+def not_so_tricky_method(a_string_param, an_array_param)
+  a_string_param += "rutabaga"
+  an_array_param += ["rutabaga"]
+
+  return a_string_param, an_array_param
+end
+
+my_string = "pumpkins"
+my_array = ["pumpkins"]
+my_string, my_array = not_so_tricky_method(my_string, my_array)
+
+puts "My string looks like this now: #{my_string}"
+puts "My array looks like this now: #{my_array}"
+=end
+
+greetings = { a: 'hi' }
+puts greetings.object_id
+informal_greetings = greetings[:a]
+puts informal_greetings.object_id
+informal_greetings << ' there'
+
+puts informal_greetings
+puts informal_greetings.object_id
+puts greetings
+puts greetings.object_id
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
