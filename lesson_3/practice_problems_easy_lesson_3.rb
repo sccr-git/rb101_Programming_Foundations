8d15dc95fa12880e708b9974f9483ffe11d81be2
sccr-git <creelins@gmail.com>
2021-08-30 07:28:15 -0400

    completed lesson 3 problems

--- lesson_3/practice_problems_easy_lesson_3.rb
+++ lesson_3/practice_problems_easy_lesson_3.rb
@@ -426,6 +426,7 @@ puts "My string looks like this now: #{my_string}"
 puts "My array looks like this now: #{my_array}"
 =end
 
+=begin
 greetings = { a: 'hi' }
 puts greetings.object_id
 informal_greetings = greetings[:a]
@@ -436,11 +437,72 @@ puts informal_greetings
 puts informal_greetings.object_id
 puts greetings
 puts greetings.object_id
+=end
 
+=begin
+def mess_with_vars(one, two, three)
+  one = two
+  two = three
+  three = one
+end
 
+one = "one"
+two = "two"
+three = "three"
 
+mess_with_vars(one, two, three)
 
+puts "one is: #{one}"
+puts "two is: #{two}"
+puts "three is: #{three}"
+=end
 
+=begin
+def mess_with_vars(one, two, three)
+  one = "two"
+  two = "three"
+  three = "one"
+end
+
+one = "one"
+two = "two"
+three = "three"
+
+mess_with_vars(one, two, three)
+
+puts "one is: #{one}"
+puts "two is: #{two}"
+puts "three is: #{three}"
+=end
+
+=begin
+def mess_with_vars(one, two, three)
+  one.gsub!("one","two")
+  two.gsub!("two","three")
+  three.gsub!("three","one")
+end
+
+one = "one"
+two = "two"
+three = "three"
+
+mess_with_vars(one, two, three)
+
+puts "one is: #{one}"
+puts "two is: #{two}"
+puts "three is: #{three}"
+=end
+
+def dot_separated_ip_address?(input_string)
+  dot_separated_words = input_string.split(".")
+  return false unless dot_separated_words.length == 4
+
+  while dot_separated_words.size > 0 do
+    word = dot_separated_words.pop
+    return false unless is_an_ip_number?(word)
+  end
+  true
+end
 
 
 
