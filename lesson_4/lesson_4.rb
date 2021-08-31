71e568f92e822e6e043ba889c2ff529822b5ca03
sccr-git <creelins@gmail.com>
2021-08-31 08:24:57 -0400

    completed lesson 4 topic 3 of looping

--- lesson_4/lesson_4.rb
+++ lesson_4/lesson_4.rb
@@ -20,9 +20,11 @@ hsh.fetch(:d)  # => KeyError: key not found: :d
                #        from /usr/bin/irb:11:in `<main>'
 =end
 
+=begin
 str = 'Patricia'
 arr = str.split('')
 p arr.join
+=end
 
 =begin
 str = 'How do you get to Carnegie Hall?'
@@ -54,3 +56,129 @@ hsh[:pear] = 'fruit'
 hsh[:broccoli] = 'vegetable'
 puts hsh # => { :apple => "Fruit", :carrot => "Produce", :pear => "Produce", :broccoli => "Produce" }
 =end
+
+=begin
+loop do
+  number = rand(1..10)   # a random number between 1 and 10
+  puts 'Hello!'
+  if number == 5
+    puts 'Exiting...'
+    break
+  end
+end
+=end
+
+=begin
+counter = 0
+
+loop do
+  puts 'hello'
+  counter += 1
+  break if counter == 5
+end
+=end
+
+=begin
+counter = 0
+
+loop do
+  counter += 1
+  next if counter.odd?
+  puts counter
+  break if counter > 5
+end
+=end
+
+=begin
+alphabit = 'abcdefghijklmnopqrstuvwxyz'
+
+counter = 0
+
+loop do
+  puts alphabit[counter]
+  counter += 1
+  break if counter >= alphabit.size
+end
+=end
+
+=begin
+colors = ['green', 'yellow', 'blue', 'orange']
+counter = 0
+
+loop do
+  break if counter >= colors.length
+  puts "I am the color #{colors[counter]}"
+  counter += 1
+end
+=end
+
+=begin
+number_of_pets = {
+  'dogs' => 2,
+  'cats' => 3,
+  'fish' => 4
+}
+
+counter = 0
+pets = number_of_pets.keys
+
+loop do
+  break if counter == number_of_pets.size
+  current_pet = pets[counter]
+  current_pet_number = number_of_pets[current_pet]
+  puts "I have #{current_pet_number} #{current_pet}"
+  counter += 1
+end
+=end
+
+number_of_pets = {
+  'dogs' => 4,
+  'cats' => 2,
+  'fish' => 1
+}
+
+
+counter = 0
+pets = number_of_pets.keys
+
+loop do
+  break if counter == number_of_pets.size
+  current_pet = pets[counter]
+  current_pet_number = number_of_pets[current_pet]
+  puts "I have #{current_pet_number} #{current_pet}"
+  counter += 1
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
